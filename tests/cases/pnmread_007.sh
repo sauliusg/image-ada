d606cc6ba81d8e2d80b44e6e04d3182b325f6114
saulius <saulius@c6fcb151-bc4f-4061-980e-97dad420485a>
2024-07-16 09:32:36 +0000

    Adding a multi-file read test.
    trunk/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/image-ada/trunk@24 c6fcb151-bc4f-4061-980e-97dad420485a

---
+++ tests/cases/pnmread_007.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./bin/pnmread \
+	tests/data/P1.pbm \
+	tests/data/P2-large.pgm \
+	tests/data/P2-max100.pgm \
+	tests/data/P2.pgm \
+	tests/data/P5-large.pgm \
+	tests/data/P5.pgm \
+	#
